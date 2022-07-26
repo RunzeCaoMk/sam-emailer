ab595a8d3444c939403b5755e828c8845659baa4
Mark C <caor@cs.uchicago.edu>
2022-07-26 11:14:17 -0400

    pass json back to sender

--- EmailFunction/src/main/java/edu/uchicago/caor/email/App.java
+++ EmailFunction/src/main/java/edu/uchicago/caor/email/App.java
@@ -14,6 +14,8 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
 
 /**
  * Handler for requests to Lambda function.
@@ -40,8 +42,13 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                     .withStatusCode(400);
         }
 
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("subject", message.getSubject());
+        jsonObject.put("body", message.getBody());
+        jsonObject.put("email", message.getEmail());
+
         return response
                 .withStatusCode(200)
-                .withBody("{}");
+                .withBody(jsonObject.toString());
     }
 }
